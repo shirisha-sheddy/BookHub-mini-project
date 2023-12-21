754f375de992c8457509e185cd25987138e9084a
shirisha-sheddy <shirishasheddy@gmail.com>
2023-12-21 11:04:02 +0000

    BookHub mini project

---
+++ src/components/BookShelves/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+
+import RightComponent from '../RightComponent'
+
+import './index.css'
+
+const BookShelves = () => (
+  <>
+    <Header Home />
+    <div className="Book-Shelves-main-container">
+      <RightComponent />
+    </div>
+  </>
+)
+
+export default BookShelves
